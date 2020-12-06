049cf98d1e2caf7e43eb50ea0c0fef0332f9206c
Weronika Kansy <weronika.kansy@gmail.com>
2020-12-06 19:11:33 +0100

    Statement added to event handler: class .btn-active added to/removed from btn on click

--- features/accordion/app.js
+++ features/accordion/app.js
@@ -2,4 +2,5 @@ const section = document.querySelector('.accordion');
 
 section.addEventListener('click', event => {
   event.target.nextElementSibling.classList.toggle('visible');
+  event.target.classList.toggle('btn-active');
 });
