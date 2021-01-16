c0db9ffef23cbecf0cc3bcbdb66cb3749e8e0e69
Weronika Kansy <weronika.kansy@gmail.com>
2021-01-16 03:24:19 +0100

    Div collapse implemented with CSS and JS

--- features/accordion/app.js
+++ features/accordion/app.js
@@ -1,6 +1,16 @@
 const section = document.querySelector('.accordion');
 
 section.addEventListener('click', event => {
-  event.target.nextElementSibling.classList.toggle('visible');
+  const div = event.target.nextElementSibling;
+  const article = document.querySelector('.accordion-article');
+
+  if (div.style.maxHeight) {
+    div.style.maxHeight = null;
+    div.style.transform = "scaleY(0)";
+  } else {
+    div.style.maxHeight = `${article.scrollHeight}px`;
+    div.style.transform = "scaleY(1)";
+  }
+
   event.target.classList.toggle('btn-active');
 });
