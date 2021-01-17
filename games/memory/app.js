d483e9827206554c5e9242510d39fee9a10093ad
Weronika Kansy <weronika.kansy@gmail.com>
2021-01-17 14:41:03 +0100

    app.js added; array with card pairs created

---
+++ games/memory/app.js
@@ -0,0 +1,50 @@
+const cardArray = [
+  {
+    name: "BoJack Horseman",
+    img: "./img/bojack.png"
+  },
+  {
+    name: "BoJack Horseman",
+    img: "./img/bojack.png"
+  },
+  {
+    name: "Charlotte",
+    img: "./img/charlotte.png"
+  },
+  {
+    name: "Charlotte",
+    img: "./img/charlotte.png"
+  },
+  {
+    name: "Princess Carolyn",
+    img: "./img/carolyn.png"
+  },
+  {
+    name: "Princess Carolyn",
+    img: "./img/carolyn.png"
+  },
+  {
+    name: "Mr Peanutbutter",
+    img: "./img/peanutbutter.png"
+  },
+  {
+    name: "Mr Peanutbutter",
+    img: "./img/peanutbutter.png"
+  },
+  {
+    name: "Diane",
+    img: "./img/diane.png"
+  },
+  {
+    name: "Diane",
+    img: "./img/diane.png"
+  },
+  {
+    name: "Hollyhock",
+    img: "./img/hollyhock.png"
+  },
+  {
+    name: "Hollyhock",
+    img: "./img/hollyhock.png"
+  }
+];
