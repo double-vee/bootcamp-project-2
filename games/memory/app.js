12c74058efeff3d0c9967b4211740288f55241d9
Weronika Kansy <weronika.kansy@gmail.com>
2021-01-17 14:49:03 +0100

    Cards added to board with forEach method

--- games/memory/app.js
+++ games/memory/app.js
@@ -53,3 +53,11 @@ const gameContainer = document.querySelector(".game-container");
 const resultDisplay = document.getElementById("result");
 const header = document.getElementById("header-score");
 const resumeGameBtn = document.getElementById("resume-game");
+
+cardArray.forEach(function(item, index) {
+  let card = document.createElement("img");
+  card.setAttribute("src", "./img/board.png");
+  card.setAttribute("data-id", index);
+  card.classList.add("card");
+  gameContainer.appendChild(card);
+})
