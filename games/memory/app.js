dbca7eaddb838107f499da0dac23d144eec88ade
Weronika Kansy <weronika.kansy@gmail.com>
2022-06-09 02:52:47 +0200

    Removed console logs and redundant alerts

--- games/memory/app.js
+++ games/memory/app.js
@@ -1,52 +1,52 @@
 const cardArray = [
   {
     name: "BoJack Horseman",
-    img: "./img/bojack.png"
+    img: "./img/bojack.png",
   },
   {
     name: "BoJack Horseman",
-    img: "./img/bojack.png"
+    img: "./img/bojack.png",
   },
   {
     name: "Charlotte",
-    img: "./img/charlotte.png"
+    img: "./img/charlotte.png",
   },
   {
     name: "Charlotte",
-    img: "./img/charlotte.png"
+    img: "./img/charlotte.png",
   },
   {
     name: "Princess Carolyn",
-    img: "./img/carolyn.png"
+    img: "./img/carolyn.png",
   },
   {
     name: "Princess Carolyn",
-    img: "./img/carolyn.png"
+    img: "./img/carolyn.png",
   },
   {
     name: "Mr Peanutbutter",
-    img: "./img/peanutbutter.png"
+    img: "./img/peanutbutter.png",
   },
   {
     name: "Mr Peanutbutter",
-    img: "./img/peanutbutter.png"
+    img: "./img/peanutbutter.png",
   },
   {
     name: "Diane",
-    img: "./img/diane.png"
+    img: "./img/diane.png",
   },
   {
     name: "Diane",
-    img: "./img/diane.png"
+    img: "./img/diane.png",
   },
   {
     name: "Hollyhock",
-    img: "./img/hollyhock.png"
+    img: "./img/hollyhock.png",
   },
   {
     name: "Hollyhock",
-    img: "./img/hollyhock.png"
-  }
+    img: "./img/hollyhock.png",
+  },
 ];
 
 cardArray.sort(() => 0.5 - Math.random());
@@ -60,25 +60,23 @@ let cardsChosen = [];
 let cardsChosenID = [];
 let cardsWon = [];
 
-cardArray.forEach(function(item, index) {
+cardArray.forEach(function (item, index) {
   let card = document.createElement("img");
   card.setAttribute("src", "./img/board.png");
   card.setAttribute("data-id", index);
   card.classList.add("card");
   gameContainer.appendChild(card);
   card.addEventListener("click", flipCard);
-})
+});
 
 function flipCard() {
   let cardID = this.getAttribute("data-id");
   cardsChosen.push(cardArray[cardID].name);
-  console.log("cardsChosen: " + cardsChosen);
   cardsChosenID.push(cardID);
-  console.log("cardsChosenID: " + cardsChosenID);
   this.setAttribute("src", cardArray[cardID].img);
-  if(cardsChosen.length === 2) {
-    setTimeout(checkForMatch, 500)
-  } 
+  if (cardsChosen.length === 2) {
+    setTimeout(checkForMatch, 500);
+  }
 }
 
 function checkForMatch() {
@@ -98,12 +96,9 @@ function checkForMatch() {
     cards[optionOneID].removeEventListener("click", flipCard);
     cards[optionTwoID].removeEventListener("click", flipCard);
     cardsWon.push(cardsChosen);
-    console.log("cards won: " + cardsWon);
-    alert("Score!");
   } else {
     cards[optionOneID].setAttribute("src", "./img/board.png");
     cards[optionTwoID].setAttribute("src", "./img/board.png");
-    alert("Try again");
   }
 
   cardsChosen = [];
