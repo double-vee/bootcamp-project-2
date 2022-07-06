5e358857be89736d5acb4ba76dbbe436d062fe2d
Weronika Kansy <weronika.kansy@gmail.com>
2022-07-06 02:39:31 +0200

    Event handler executed only when a button has been clicked

--- features/accordion/app.js
+++ features/accordion/app.js
@@ -1,16 +1,18 @@
-const section = document.querySelector('.accordion');
+const section = document.querySelector(".accordion");
 
-section.addEventListener('click', event => {
-  const div = event.target.nextElementSibling;
-  const article = document.querySelector('.accordion-article');
+section.addEventListener("click", (event) => {
+  if (event.target.classList.contains("accordion-btn")) {
+    let div = event.target.nextElementSibling;
+    let article = event.target.nextElementSibling.firstElementChild;
 
-  if (div.style.maxHeight) {
-    div.style.maxHeight = null;
-    div.style.transform = "scaleY(0)";
-  } else {
-    div.style.maxHeight = `${article.scrollHeight}px`;
-    div.style.transform = "scaleY(1)";
+    if (div.style.maxHeight) {
+      div.style.maxHeight = null;
+      div.style.transform = "scaleY(0)";
+    } else {
+      div.style.maxHeight = `${article.scrollHeight}px`;
+      div.style.transform = "scaleY(1)";
+    }
+
+    event.target.classList.toggle("btn-active");
   }
-
-  event.target.classList.toggle('btn-active');
 });
